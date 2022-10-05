0908c8a589edc74ea240a8a0400f6e61cb208096
Jun Shimura <junshimura@gmail.com>
2022-10-05 10:35:54 +0900

    1st

---
+++ C_Sharp_Variable/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace C_Sharp_Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, k;
+            double height;  // 身長 
+            double weight; // 体重
+            double bmi;     // BMI
+
+            //std::cout << "Hello World!\n";
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
