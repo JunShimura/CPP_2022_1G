c592c919c71bf9c32ec17a4206ac8a677492fca4
Jun Shimura <junshimura@gmail.com>
2022-10-12 09:29:04 +0900

    ex01

---
+++ Chapter3/Chapter3.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	const int STD_BMI = 22;		// 標準BMI
+	const double THIN_BMI = 18.5;	// 痩せすぎBMI
+	const double FAT1_BMI = 25;		// 肥満度1
+	const double FAT2_BMI = 30;		// 肥満度2
+	const double FAT3_BMI = 35;		// 肥満度3
+	const double FAT4_BMI = 40;		// 肥満度4
+
+	const int meterPerCenti = 100;	//1mで何㎝か
+	double height;  // 身長 
+	double weight; // 体重
+	double bmi;     // BMI
+	double stdWeight;	// 標準体重
+
+	//　キー入力をheightに格納
+	cout << "身長(cm)を入力してください";
+	cin >> height;
+	//　キー入力をweightに格納
+	cout << "体重(kg)を入力してください";
+	cin >> weight;
+
+	// 身長をcm単位からｍ単位に変換する
+	height /= meterPerCenti;
+
+	// 身長と体重からBMIを算出する
+	bmi = weight / height / height;
+
+	//　標準体重を計算する
+	stdWeight = STD_BMI * height * height;
+
+	// BMIを画面に表示する
+	std::cout << "あなたのBMIは" << fixed << setprecision(1) << bmi << "です。" << endl;
+
+	// 肥満かどうかを判定した結果を表示する
+	if (bmi < THIN_BMI) {
+		cout << "低体重です" << endl;
+	}
+	else if ( bmi < FAT1_BMI) {
+		cout << "普通体重です" << endl;
+	}
+	else if (bmi < FAT2_BMI) {
+		cout << "肥満度1です" << endl;
+	}
+	else if (bmi < FAT3_BMI) {
+		cout << "肥満度2です" << endl;
+	}
+	else if (bmi < FAT4_BMI) {
+		cout << "肥満度3です" << endl;
+	}
+	else {
+		cout << "肥満度4です" << endl;
+	}
+	
+	// 標準体重を画面に表示する
+	std::cout << "あなたの標準体重は" << fixed << setprecision(1) << stdWeight << "です。" << endl;
+}
---
+++ Chapter3_switchCase/Chapter3_switchCase.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int channel;		// チャンネルの番号
+    string stationName;	// テレビ局名
+
+    // キー入力をchannelに格納する
+    cout << "チャンネル番号を入力してください：";
+    cin >> channel;
+    // テレビ局名をstationNameに格納する
+    switch(channel) {
+    case 1:
+        stationName = "東北放送";
+        break;
+    case 2:
+        stationName = "NHK教育";
+        break;
+    case 3:
+        stationName = "NHK総合";
+        break;
+    case 4:
+        stationName = "ミヤギテレビ";
+        break;
+    case 5:
+        stationName = "東日本放送";
+        break;
+   case 8:
+        stationName = "仙台放送";
+        break;
+   default:
+       stationName = "割り当てなし";
+       break;
+    }
+
+    /* ifの例
+
+    // テレビ局名をstationNameに格納する
+    if (channel == 1) {
+        stationName = "東北放送";
+    }
+    else if (channel == 2) {
+        stationName = "NHK教育";
+    }
+    else if (channel == 3) {
+        stationName = "NHK総合";
+    }
+    else if (channel == 4) {
+        stationName = "ミヤギテレビ";
+    }
+    else if (channel == 5) {
+        stationName = "東日本放送";
+    }
+    else if (channel == 8) {
+        stationName = "仙台放送";
+    }
+    else {
+        stationName = "割り当てなし";
+    }
+    */
+
+    // テレビ局名を表示する
+    cout << stationName << endl;
+
+    return 0;
+}
